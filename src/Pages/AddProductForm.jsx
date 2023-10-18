0e14bf4542828ec7ad1d2c2a9a81536aa05a5490
ranitsarker <rntprince@gmail.com>
2023-10-18 09:32:26 +0600

    frontend product form data has sent to server

--- src/Pages/AddProductForm.jsx
+++ src/Pages/AddProductForm.jsx
@@ -21,6 +21,18 @@ const AddProductForm = () => {
       };
   
       console.log(newProduct);
+      // send form data to server 
+      fetch('http://localhost:5000/product' , {
+        method: 'POST',
+        headers:{
+            'content-type' : 'application/json',
+        },
+        body: JSON.stringify(newProduct)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
     };
   
 
