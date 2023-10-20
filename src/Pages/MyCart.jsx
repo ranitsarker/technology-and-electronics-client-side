30af73c1071a8401dc2a89eb9f8eead1ff49f253
ranitsarker <rntprince@gmail.com>
2023-10-21 03:43:13 +0600

    mycart page design updated

--- src/Pages/MyCart.jsx
+++ src/Pages/MyCart.jsx
@@ -19,20 +19,29 @@ function MyCart() {
   }, [user]);
 
   return (
-    <div>
-      <h1>My Cart</h1>
-      {cartData.length > 0 ? (
-        cartData.map((item, index) => (
-          <div key={index}>
-            <img src={item.product.image} alt="Product" />
-            <p>Name: {item.product.name}</p>
-            <p>Price: ${item.product.price}</p>
+<div className='md:mx-10 mx-4 my-8'>
+  <h1 className="text-2xl font-semibold mb-4 ">My Cart</h1>
+  {cartData.length > 0 ? (
+    <div className="grid grid-cols-1 gap-4">
+      {cartData.map((item, index) => (
+        <div key={index} className="flex items-center border p-2">
+          <img
+            src={item.product.image}
+            alt="Product"
+            className="w-16 h-16 object-contain rounded-md mr-4"
+          />
+          <div>
+            <p className="text-lg font-semibold">{item.product.name}</p>
+            <p className="text-gray-600">Price: ${item.product.price}</p>
           </div>
-        ))
-      ) : (
-        <p>Your cart is empty.</p>
-      )}
+        </div>
+      ))}
     </div>
+  ) : (
+    <p className="text-gray-600">Your cart is empty.</p>
+  )}
+</div>
+
   );
 }
 
